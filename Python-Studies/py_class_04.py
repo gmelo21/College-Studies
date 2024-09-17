3ae4618547b659cf04efdd899fc4f6a28c578504
Guilherme Melo <160745486+gmelo21@users.noreply.github.com>
2024-09-17 09:29:55 -0300

    Updated py_class_04.py

--- Python-Studies/py_class_04.py
+++ Python-Studies/py_class_04.py
@@ -5,22 +5,24 @@ thingamajigs = {}
 thingamajangs = {}
 thingamabonks = {"Thong": "A bajillion thongies.", "Thung": "A zillion thungies."}
 
+# Adding two keys to the first dictionary:
 for n in range(2):
-    thing = input("Choose a thing: ").capitalize().strip()
-    thingies = input("How many thingies? ").capitalize().strip()
-    thingamajigs[thing] = thingies
+    thing = input("Choose a thing: ").capitalize().strip()   # Key for the first dictionary.
+    thingies = input("How many thingies? ").capitalize().strip()   # Item for the key in the first dictionary.
+    thingamajigs[thing] = thingies    # Adds it to the dictionary.
 
 print()
 
+# Adding two keys to the second dictionary:
 for n in range(2):
-    thang = input("Choose a thang: ").capitalize().strip()
-    thangies = input("How many thangies? ").capitalize().strip()
-    thingamajangs[thang] = thangies
+    thang = input("Choose a thang: ").capitalize().strip()   # Key for the second dictionary.
+    thangies = input("How many thangies? ").capitalize().strip()   # Item for the key in the second dictionary.
+    thingamajangs[thang] = thangies    # Adds it to the dictionary.
 
 thingaminator = {1: thingamajigs,
                  2: thingamajangs,
                  3: thingamabonks
-                 }
+                 }    # Dictionary which stores the two dictionaries we created, plus one more that was already here.
 
 print("\nThese are your dictionaries: ")
 
@@ -30,7 +32,7 @@ print(f"3 - {thingaminator[3]}")
 
 print("\nWhy don't you try to print one of the things/thangs, in one of the dictionaries? ")
 
-selectionDictionary = input("Type in the number of one of the dictionaries: ").capitalize().strip()
-selectionItem = input("Type in the name of one of the items: ").capitalize().strip()
+selectionDictionary = input("Type in the number of one of the dictionaries: ").strip()    # Select one of the dictionary numbers.
+selectionItem = input("Type in the name of one of the items: ").capitalize().strip()   # Select one of the items by their name.
 
 print(f"The thing \"{selectionItem}\" has \"{thingaminator[int(selectionDictionary)][selectionItem]}\" itemies!")
