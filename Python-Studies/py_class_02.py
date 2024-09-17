79f809672760daed3bdba53b619687314540a8ce
Guilherme Melo <160745486+gmelo21@users.noreply.github.com>
2024-09-17 09:25:09 -0300

    Updated py_class_02.py

--- Python-Studies/py_class_02.py
+++ Python-Studies/py_class_02.py
@@ -2,8 +2,8 @@
 # But I want you to know it was there, so I don't look lazy for the small code.
 
 # Function to repeat the desired text for desired length.
-def repeat_for_length(text, wantedLength):
-    return (text * (wantedLength//len(text) + 1))[:wantedLength:]
+def repeat_for_length(text, wanted_length):
+    return (text * (wanted_length//len(text) + 1))[:wanted_length:]
     # The "+1" is here so the expression always results in the exact number or above (which would be cut by slicing).
     # Easier than using math.ceil, or a condition to see if the "+1" would be necessary or not.
 
