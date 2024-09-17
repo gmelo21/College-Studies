7e404c41f57d8d98e82b37b2954aae93cd8b999f
Guilherme Melo <160745486+gmelo21@users.noreply.github.com>
2024-09-17 09:53:49 -0300

    Update py_class_07.py

--- Python-Studies/py_class_07.py
+++ Python-Studies/py_class_07.py
@@ -1,3 +1,5 @@
+# Sep 15, 2024.
+
 # Hey all. Just as a heads-up, this program is going to create two text files in your desktop.
 # Nothing big, easy to delete, and most importantly, only contains a bit of malware (joke).
 # As you probably guessed, we're just studying text files manipulation. Stuff is getting interesting!
