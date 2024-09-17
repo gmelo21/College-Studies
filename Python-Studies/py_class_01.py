3f517d1bf98a8a014781202135022eb33a44af39
Guilherme Melo <160745486+gmelo21@users.noreply.github.com>
2024-09-17 09:24:42 -0300

    Updated py_class_01.py

--- Python-Studies/py_class_01.py
+++ Python-Studies/py_class_01.py
@@ -1,5 +1,7 @@
 # Working with functions and function-related information, like docstrings, type hints, parameters, and so on.
+# Let's calculate your K/D/A score.
 
+# Function to run the calculations.
 def calculate_battle_score(kills: int = 0, deaths: int = 0, assists: int = 0) -> float:
     """
     Args:
@@ -11,11 +13,10 @@ def calculate_battle_score(kills: int = 0, deaths: int = 0, assists: int = 0) ->
         (float): Average battle score.
     """
 
-    if deaths == 0:
-        battle_score = kills + (assists / 2)
+    battle_score = kills + (assists / 2)
 
-    else:
-        battle_score = (kills + (assists / 2)) / deaths
+    if deaths > 0:
+        battle_score = battle_score / deaths
 
     return battle_score
 
@@ -26,10 +27,10 @@ while True:
     results = input("Please type your K/D/A for the match, separating the numbers with a slash (/): ")
     results = results.replace(" ", "").split("/")
 
-    if all(x.isdigit() for x in results):
-        results = [int(x) for x in results]
+    if all(x.isdigit() for x in results):    # Verifies if all values inputted are positive numbers.
+        results = [int(x) for x in results]    # Appends the numbers separately to the list.
 
-        if len(results) == 3:
+        if len(results) == 3:    # Checks if there are only 3 values in the list.
             print(f"Player {username} has finished a match with a battle score of {calculate_battle_score(results[0], results[1], results[2]):.2f}.")
             break
 
