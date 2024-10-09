d398cb4fa23a50300b55fa67a5211209305cd31e
Melo <guilhermecunhamelo2105@gmail.com>
2024-10-09 15:47:52 -0300

    Finished main code and functions

--- Python-Studies/py_class_09.py
+++ Python-Studies/py_class_09.py
@@ -2,6 +2,9 @@ import requests
 
 
 def fetch_data(url):
+    if url is None:
+        return None
+
     try:
         response = requests.get(url)
 
@@ -12,12 +15,14 @@ def fetch_data(url):
             print("Requisition error.")
             return None
 
-    except Exception as error:
-        print(f"Error: {error}")
+    except:
         return None
 
 
-def list_pokemon(pokemon_list):
+def list_pokemon(pokemon_list, currentPage, totalPages):
+    print(f"Page {currentPage} / {totalPages}")
+    print()
+
     for pokemon in pokemon_list:
         print(pokemon["name"].capitalize())
 
@@ -33,28 +38,71 @@ def show_abilities(pokemon_url):
             print(ability["ability"]["name"].capitalize())
 
 
-mainUrl = "https://pokeapi.co/api/v2/pokemon/"
+def find_pokemon_across_pages(name):
+    mainUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+    while mainUrl:
+        dictResponse = fetch_data(mainUrl)
+
+        if dictResponse:
+            for pokemon in dictResponse["results"]:
+                if pokemon["name"].lower() == name:
+                    print()
+                    show_abilities(pokemon["url"])
+                    return
+
+            mainUrl = dictResponse["next"]
+
+        else:
+            break
+
+    print("Pokémon not found.")
+    print()
+    return None
+
+
+offset = 0
+limit = 15
+
+mainUrl = f"https://pokeapi.co/api/v2/pokemon/?limit=15"
+currentPage = 1
+totalPages = 0
 
 while True:
     dictResponse = fetch_data(mainUrl)
 
     if dictResponse:
-        list_pokemon(dictResponse["results"])
+        total_count = dictResponse["count"]
+        totalPages = (total_count // limit) + \
+            (1 if total_count % limit > 0 else 0)
+        list_pokemon(dictResponse["results"], currentPage, totalPages)
         choice = get_user_choice()
         print()
 
         if choice == "next":
             mainUrl = dictResponse["next"]
-            
+            currentPage += 1
+
         elif choice == "previous":
             mainUrl = dictResponse["previous"]
+            currentPage -= 1
 
         else:
             for pokemon in dictResponse["results"]:
-                if pokemon["name"] == choice:
+                if pokemon["name"].lower() == choice:
                     show_abilities(pokemon["url"])
                     exit()
 
             else:
-                print("Name not found.")
-                exit()
+                searchAll = input(
+                    "Pokémon not found. Search in whole database? It may take a while. ( Y/N ) ").lower().strip()
+
+                if searchAll == "y":
+                    find_pokemon_across_pages(choice)
+                    exit()
+
+                else:
+                    exit()
+    else:
+        print("Failed to fetch data from the Pokémon API.")
+        break
