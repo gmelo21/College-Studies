3fbc7f0b9ad433fcda5a1ab844180a953d53fbdc
Melo <guilhermecunhamelo2105@gmail.com>
2024-10-22 19:12:23 -0300

    Made footer always stay in the end of the page

--- Web-Development-Studies/web_class_07/src/App.jsx
+++ Web-Development-Studies/web_class_07/src/App.jsx
@@ -1,16 +1,24 @@
-import { Outlet } from 'react-router-dom'
-import Nav from './components/Nav/Nav'
-import Footer from './components/Footer/Footer'
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+import "./app.css";
 
 function App() {
-
   return (
     <>
-      <Nav />
-      <Outlet />
-      <Footer />
+      <div id="container-page">
+        <div id="header-page">
+          <Nav />
+        </div>
+        <div id="main-page">
+          <Outlet id="outlet-page" />
+        </div>
+        <div id="footer-page">
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
