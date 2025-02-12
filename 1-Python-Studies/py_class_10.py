5fb8ce900ab1457a16d227984dffc87f6dd8b8c8
Melo <guilhermecunhamelo2105@gmail.com>
2025-02-12 18:37:31 -0300

    Finished py_class_10.py, finally

--- 1-Python-Studies/py_class_10.py
+++ 1-Python-Studies/py_class_10.py
@@ -1,67 +1,147 @@
-# Sum of all elements in a list using recursion:
+# Sum of all elements in a list using recursion
 def list_sum(chosenList):
     if len(chosenList) == 1:
-        return chosenList[0]  # Base case: when there's only one element, return it
+        # Base case: when there's only one element, return it
+        return chosenList[0]
     else:
-        return chosenList[0] + list_sum(chosenList[1:])  # Add first element and recurse with the rest of the list
+        # Add first element and recurse with the rest of the list
+        return chosenList[0] + list_sum(chosenList[1:])
 
-# Get list input from the user:
-yourList = []
-yourLength = int(input("Choose the desired length for your list: (Keep it short to make it easier to check!) "))
-
-# Populate the list with user input:
-for i in range(0, yourLength):
-    yourNumber = float(input(f"({i + 1}/{yourLength}) Choose a number to be added to your list: "))
-    yourList.append(yourNumber)
-
-# Print the sum of the list:
-print(f"The sum of all numbers in the list is: {list_sum(yourList)}\n")
+# Sum of numbers from 0 to N using recursion
 
 
-# Sum of numbers from 0 to N using recursion:
 def number_sum(chosenNumber):
     if chosenNumber == 1:
         return 1  # Base case: when the number is 1, return 1
     else:
-        return chosenNumber + number_sum(chosenNumber - 1)  # Add number to the sum of numbers below it
+        # Add number to the sum of numbers below it
+        return chosenNumber + number_sum(chosenNumber - 1)
 
-# Get number input from the user:
-yourNumber = int(input(f"Choose a number: "))
-
-# Print the sum from 0 to the chosen number:
-print(f"The sum of numbers from 0 to {yourNumber} is: {number_sum(yourNumber)}\n")
+# GCD of two numbers using recursion
 
 
-# GCD of two numbers using recursion:
 def gcd_calculator(x, y):
     if x >= y and x % y == 0:
         return y  # Base case: when x is divisible by y, return y
     elif x < y:
         return gcd_calculator(y, x)  # Swap x and y if x is less than y
     else:
-        return gcd_calculator(y, x % y)  # Recurse with the remainder of x divided by y
+        # Recurse with the remainder of x divided by y
+        return gcd_calculator(y, x % y)
 
-# Get user input for two numbers:
-yourX = int(input('Enter X\'s value: '))
-yourY = int(input('Enter Y\'s value: '))
-
-# Print the GCD of the two numbers:
-print(f"The GCD of {yourX} and {yourY} is: {gcd_calculator(yourX, yourY)}\n")
+# Exponentiation of a number using recursion
 
 
-# Exponentiation of a number using recursion:
 def exp_calculator(x, y):
     if y == 0:
         return 1  # Base case: x^0 is 1
     elif y % 2 == 0:
-        half_power = exp_calculator(x, y // 2)  # Recurse with half the exponent
-        return half_power * half_power  # Multiply the half powers together for even exponents
+        # Recurse with half the exponent
+        half_power = exp_calculator(x, y // 2)
+        # Multiply the half powers together for even exponents
+        return half_power * half_power
     else:
-        return x * exp_calculator(x, y - 1)  # Multiply x by the result of exp(x, y-1) for odd exponents
+        # Multiply x by the result of exp(x, y-1) for odd exponents
+        return x * exp_calculator(x, y - 1)
 
-# Get base and exponent input from the user:
-yourX = int(input('Enter base value: '))
-yourY = int(input('Enter exponent value: '))
+# Finding number in list
 
-# Print the result of exponentiation:
-print(f"The result of {yourX}^{yourY} is: {exp_calculator(yourX, yourY)}\n")
+
+def number_finder(chosenList, n):
+    if len(chosenList) == 0:  # Base case: if the list is empty, return 0 (number not found)
+        return 0
+    # If the first element matches the number, count it and recurse for the rest of the list
+    if chosenList[0] == n:
+        return 1 + number_finder(chosenList[1:], n)
+    # Recurse for the rest of the list if the first element doesn't match the number
+    return number_finder(chosenList[1:], n)
+
+# Function to get list input from the user
+
+
+def get_list_input():
+    yourList = []
+    yourLength = int(input("Choose the desired length for your list: "))
+    for i in range(0, yourLength):
+        yourNumber = float(
+            input(f"({i + 1}/{yourLength}) Choose a number to be added to your list: "))
+        yourList.append(yourNumber)
+    return yourList
+
+# Function to calculate sum of all elements in a list
+
+
+def sum_list():
+    yourList = get_list_input()
+    print(f"The sum of all numbers in the list is: {list_sum(yourList)}\n")
+
+# Function to calculate sum of numbers from 0 to N
+
+
+def sum_numbers():
+    yourNumber = int(input("Choose a number: "))
+    print(
+        f"The sum of numbers from 0 to {yourNumber} is: {number_sum(yourNumber)}\n")
+
+# Function to calculate GCD of two numbers
+
+
+def find_gcd():
+    yourX = int(input('Enter X\'s value: '))
+    yourY = int(input('Enter Y\'s value: '))
+    print(
+        f"The GCD of {yourX} and {yourY} is: {gcd_calculator(yourX, yourY)}\n")
+
+# Function to calculate exponentiation of a number
+
+
+def calculate_exp():
+    yourX = int(input('Enter base value: '))
+    yourY = int(input('Enter exponent value: '))
+    print(
+        f"The result of {yourX}^{yourY} is: {exp_calculator(yourX, yourY)}\n")
+
+# Function to find a number in a list
+
+
+def find_number():
+    yourList = get_list_input()
+    yourNumber = int(input("Choose the number to be searched for: "))
+    print(
+        f"\nThe number {yourNumber} appears {number_finder(yourList, yourNumber)} time(s) in your list.\n")
+
+# Menu function to allow user to select which function to run
+
+
+def menu():
+    while True:
+        print("\nSelect an option:")
+        print("1. Sum of all elements in a list")
+        print("2. Sum of numbers from 0 to N")
+        print("3. Find GCD of two numbers")
+        print("4. Exponentiation of a number")
+        print("5. Find number occurrences in a list")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+        print()
+
+        if choice == '1':
+            sum_list()
+        elif choice == '2':
+            sum_numbers()
+        elif choice == '3':
+            find_gcd()
+        elif choice == '4':
+            calculate_exp()
+        elif choice == '5':
+            find_number()
+        elif choice == '6':
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid choice. Please select again.")
+
+
+# Run the menu function
+menu()
