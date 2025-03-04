c6bc1615c7a7700fb0bf4e486a49f2b9f85bc8c6
Melo <guilhermecunhamelo2105@gmail.com>
2025-03-04 17:25:27 -0300

    Removed leftover code and properly addressed the file as py_class_11.py

--- 1-Python-Studies/py_class_11.py
+++ 1-Python-Studies/py_class_11.py
@@ -2,16 +2,13 @@ import json
 
 
 def print_family_names(family_member, level=0):
-    # Print the name of the current family member with indentation
     print(' | ' * level + family_member['name'])
 
-    # If the family member has children, recursively print their names with increased indentation
     if 'children' in family_member:
         for child in family_member['children']:
             print_family_names(child, level + 1)
 
 
-# Example JSON family tree
 family_tree_json = '''
 [
   {
@@ -41,11 +38,9 @@ family_tree_json = '''
 ]
 '''
 
-# Parse the JSON data
 family_tree = json.loads(family_tree_json)
 print()
 
-# Call the function to print all family members' names for each family
 for index, family in enumerate(family_tree):
     print_family_names(family)
     if index < len(family_tree) - 1:
